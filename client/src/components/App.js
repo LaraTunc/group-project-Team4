2e6d88ee183fbb16f1709692e057dd461c9812ea
Lara Tunc <laratunc@gmail.com>
2021-02-09 19:34:10 -0500

    Revert "added switch router and react router dom"
    This reverts commit c60635d112862d674dbe78c745fe861c40077d17.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router,
-  Switch,
-  Route } from 'react-router-dom';
 
 function App() {
   const [bacon, setBacon] = useState(null);
@@ -12,21 +9,7 @@ function App() {
       .then(data => setBacon(data));
   }, []);
 
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <div>Item Grid {bacon}</div>
-        </Route>
-        <Route path="/product/:productId">
-          <div>Single Item</div>
-        </Route>
-        <Route path="/order">
-          <div>order</div>
-        </Route>
-      </Switch>
-    </Router>
-  );
+  return <div>{bacon ? bacon : `...where's my stuff?...`}</div>;
 }
 
 export default App;
