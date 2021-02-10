014e4a7a606757ab28c338deefca9d9c80e10264
Mallory Belgica <mallorybelgica@gmail.com>
2021-02-09 21:52:21 -0500

    Combined reducers in reducer file and updated store-setup

---
+++ client/src/reducers/itemGrid-reducer.js
@@ -0,0 +1,32 @@
+const initialState = {
+    status: 'idle',
+    currentItems: null,
+    error: null,
+}
+
+export default function itemGridReducer(state = initialState, action) {
+    switch(action.type) {
+        case 'FETCH-ITEMS-DATA': {
+            return {
+                ...state,
+                status: 'loading',
+            }
+        }
+        case 'RECEIVE-ITEMS-DATA': {
+            return {
+                ...state,
+                status: 'idle',
+                currentItems: action.currentItems,
+            }
+        }
+        case 'ERROR-ITEMS-DATA': {
+            return{
+                ...state,
+                status: 'error',
+            }
+        }
+        default: {
+            return state;
+        }
+    }
+}
