cec047f929f03f05c3ef38ff1685c55886958a50
Mallory Belgica <mallorybelgica@gmail.com>
2021-02-18 20:58:52 -0500

    Added add to cart button on item grid

--- client/src/components/ItemCard.js
+++ client/src/components/ItemCard.js
@@ -1,7 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import { useDispatch } from "react-redux";
+import { addItem } from "../actions";
 
 const ItemCard = ({ item, company }) => {
+  const dispatch = useDispatch();
+
   return (
     <ItemWrapper key={item._id}>
       <ItemLink href={`/product/${item._id}`} />
@@ -9,6 +13,7 @@ const ItemCard = ({ item, company }) => {
       <Brand key={item.companyId}>{company?.name}</Brand>
       <Description>{item.name}</Description>
       <Price>{item.price}</Price>
+      <Button onClick={() => dispatch(addItem(item))}>Add to cart</Button>
     </ItemWrapper>
   );
 };
@@ -16,7 +21,11 @@ const ItemCard = ({ item, company }) => {
 export default ItemCard;
 
 const ItemWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   position: relative;
+  text-align: left;
   padding: 15px;
 `;
 
@@ -27,7 +36,7 @@ const ItemLink = styled.a`
   top: 0;
   left: 0;
   text-decoration: none;
-  z-index: 10;
+  z-index: 1;
   background-color: #fff;
   opacity: 0;
 `;
@@ -35,12 +44,23 @@ const ItemLink = styled.a`
 const Image = styled.img`
   display: block;
   max-width: 100%;
-  min-height: 180px;
-  padding-bottom: 5px;
+  max-height: 250px;
+  padding: 5px;
 `;
 
 const Brand = styled.p`
   color: #000;
+  font-weight: bold;
+  padding: 5px;
+`;
+
+const Button = styled.button`
+  position: relative;
+  z-index: 10;
+  bottom: 0;
+  background-color: #6565ee;
+  color: white;
+  padding: 10px;
   font-weight: bold;
 `;
 
